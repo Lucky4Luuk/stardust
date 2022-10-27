30dd18c0476521fde49d83c33cf37a485698716d
Luuk van Oijen <lazyluuk.channel@gmail.com>
2022-10-28 00:30:28 +0200

    Update lib.rs

--- stardust_magica_voxel/src/lib.rs
+++ stardust_magica_voxel/src/lib.rs
@@ -19,23 +19,54 @@ impl std::fmt::Display for MagicaVoxelError {
 impl std::error::Error for MagicaVoxelError {}
 
 pub struct MagicaVoxelModel {
-    model: DotVoxData,
+    voxels: Vec<(IVec3, Voxel)>,
+    min: IVec3,
+    max: IVec3,
 }
 
 impl<V: IsVoxel> IsModel<V> for MagicaVoxelModel {
     fn from_path(path: &str) -> Result<Self> {
         let model = dot_vox::load(path).map_err(|e| MagicaVoxelError(e.to_string()))?;
 
+        let mut voxels = Vec::new();
+        let mut min = ivec3(0,0,0);
+        let mut max = ivec3(0,0,0);
+
+        for node in &model.scenes {
+            match node {
+                SceneNode::Transform { attributes: _, frames, child, layer_id: _ } => {
+                    let child_node = &model.scenes[*child as usize];
+                    let pos = if let Some(frame) = frames.get(0) {
+                        frame.position().map(|p| ivec3(p.x,p.y,p.z)).unwrap_or(ivec3(0,0,0))
+                    } else { ivec3(0,0,0) };
+                    match child_node {
+                        SceneNode::Shape { attributes: _, models } => {
+                            for smodel in models {
+                                let vmodel = &model.models[smodel.model_id as usize];
+                                for voxel in &vmodel.voxels {
+                                    let vpos = ivec3(voxel.x.into(),voxel.y.into(),voxel.z.into());
+                                    min = min.min(vpos);
+                                    max = max.max(vpos);
+                                    voxels.push((pos + vpos, *voxel));
+                                }
+                            }
+                        },
+                        _ => panic!("Wtf?"),
+                    }
+                },
+                _ => {}, // Don't handle this
+            }
+        }
+
         Ok(Self {
-            model: model,
+            voxels: voxels,
+            min: min,
+            max: max,
         })
     }
 
     fn voxel_bounds(&self) -> (IVec3, IVec3) {
-        let mut min = ivec3(0,0,0);
-        let mut max = ivec3(0,0,0);
-
-        (min, max)
+        (self.min, self.max)
     }
 
     fn get_voxel(&self, pos: IVec3) -> V {
